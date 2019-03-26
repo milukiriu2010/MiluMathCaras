6cf45f290c7b868c9d554c4c50b03bbfe03bac51
milukiriu2010 <milu.kiriu2010@gmail.com>
2019-03-26 20:46:19 +0900

    square<=>diamond

--- app/src/main/java/milu/kiriu2010/milumathcaras/gui/draw/polygon/square/Square2Diamond01Drawable.kt
+++ app/src/main/java/milu/kiriu2010/milumathcaras/gui/draw/polygon/square/Square2Diamond01Drawable.kt
@@ -2,7 +2,6 @@ package milu.kiriu2010.milumathcaras.gui.draw.polygon.square
 
 import android.graphics.*
 import android.os.Handler
-import android.util.Log
 import milu.kiriu2010.gui.basic.MyPointF
 import milu.kiriu2010.milumathcaras.gui.draw.MyDrawable
 import milu.kiriu2010.milumathcaras.gui.main.NotifyCallback
@@ -47,12 +46,15 @@ class Square2Diamond01Drawable: MyDrawable() {
     private val sideE = side/split
 
     // 分裂モード
-    private var dirSplit = ModeSplit.VERTICAL
+    private var dirSplit = ModeSplit.HORIZONTAL
 
     // 1ターン内の移動比率
     private var ratioNow = 0f
     private val ratioDiv = 0.1f
     private val ratioMax = 1f
+
+    // "描画点の初期位置設定"の実施回数
+    private var nCnt = 0
 
     // 描画点のリスト
     //   12点
@@ -87,7 +89,7 @@ class Square2Diamond01Drawable: MyDrawable() {
     // 矢印を描くペイント
     // -------------------------------
     private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.RED
+        color = 0xff19b5fe.toInt()
         style = Paint.Style.FILL
     }
 
@@ -135,7 +137,7 @@ class Square2Diamond01Drawable: MyDrawable() {
                     // 描画
                     invalidateSelf()
 
-                    handler.postDelayed(runnable, 100)
+                    handler.postDelayed(runnable, 50)
                 }
                 // "停止"状態のときは、更新されないよう処理をスキップする
                 else {
@@ -173,11 +175,16 @@ class Square2Diamond01Drawable: MyDrawable() {
         vertexLst.clear()
 
         // 分裂方向を切り替える
-        dirSplit = if ( dirSplit == ModeSplit.VERTICAL) ModeSplit.HORIZONTAL else ModeSplit.VERTICAL
+        if (nCnt > 1) {
+            dirSplit = if (dirSplit == ModeSplit.VERTICAL) ModeSplit.HORIZONTAL else ModeSplit.VERTICAL
+        }
 
         // 分裂方向に合わせて、描画点の初期位置・移動方向を決定
         when (dirSplit) {
             ModeSplit.HORIZONTAL -> {
+                // ---------------------------------------------
+                // 右半分
+                // ---------------------------------------------
                 val vertex0 = Vertex().also {
                     it.d = Direction.RIGHT
                     it.p = MyPointF(sideE*0.5f,0f)
@@ -203,7 +210,76 @@ class Square2Diamond01Drawable: MyDrawable() {
                     it.p = MyPointF(sideE*0.5f,0f)
                 }
 
+                vertexLst.add(vertex0)
+                vertexLst.add(vertex1)
+                vertexLst.add(vertex2)
+                vertexLst.add(vertex3)
+                vertexLst.add(vertex4)
+                vertexLst.add(vertex5)
 
+                // ---------------------------------------------
+                // 左半分
+                // ---------------------------------------------
+                val vertex6 = Vertex().also {
+                    it.d = Direction.LEFT
+                    it.p = MyPointF(-sideE*0.5f,0f)
+                }
+                val vertex7 = Vertex().also {
+                    it.d = Direction.STILL
+                    it.p = MyPointF(-sideE*0.5f,-sideE*0.5f)
+                }
+                val vertex8 = Vertex().also {
+                    it.d = Direction.UP
+                    it.p = MyPointF(0f,-sideE*0.5f)
+                }
+                val vertex9 = Vertex().also {
+                    it.d = Direction.DOWN
+                    it.p = MyPointF(0f,sideE*0.5f)
+                }
+                val vertex10 = Vertex().also {
+                    it.d = Direction.STILL
+                    it.p = MyPointF(-sideE*0.5f,sideE*0.5f)
+                }
+                val vertex11 = Vertex().also {
+                    it.d = Direction.LEFT
+                    it.p = MyPointF(-sideE*0.5f,0f)
+                }
+
+                vertexLst.add(vertex6)
+                vertexLst.add(vertex7)
+                vertexLst.add(vertex8)
+                vertexLst.add(vertex9)
+                vertexLst.add(vertex10)
+                vertexLst.add(vertex11)
+            }
+            ModeSplit.VERTICAL -> {
+                // ---------------------------------------------
+                // 上半分
+                // ---------------------------------------------
+                val vertex0 = Vertex().also {
+                    it.d = Direction.UP
+                    it.p = MyPointF(0f, sideE*0.5f)
+                }
+                val vertex1 = Vertex().also {
+                    it.d = Direction.STILL
+                    it.p = MyPointF(-sideE*0.5f,sideE*0.5f)
+                }
+                val vertex2 = Vertex().also {
+                    it.d = Direction.RIGHT
+                    it.p = MyPointF(-sideE*0.5f,0f)
+                }
+                val vertex3 = Vertex().also {
+                    it.d = Direction.LEFT
+                    it.p = MyPointF(sideE*0.5f,0f)
+                }
+                val vertex4 = Vertex().also {
+                    it.d = Direction.STILL
+                    it.p = MyPointF(sideE*0.5f,sideE*0.5f)
+                }
+                val vertex5 = Vertex().also {
+                    it.d = Direction.UP
+                    it.p = MyPointF(0f, sideE*0.5f)
+                }
 
                 vertexLst.add(vertex0)
                 vertexLst.add(vertex1)
@@ -211,19 +287,100 @@ class Square2Diamond01Drawable: MyDrawable() {
                 vertexLst.add(vertex3)
                 vertexLst.add(vertex4)
                 vertexLst.add(vertex5)
-            }
-            ModeSplit.VERTICAL -> {
 
+                // ---------------------------------------------
+                // 下半分
+                // ---------------------------------------------
+                val vertex6 = Vertex().also {
+                    it.d = Direction.DOWN
+                    it.p = MyPointF(0f,-sideE*0.5f)
+                }
+                val vertex7 = Vertex().also {
+                    it.d = Direction.STILL
+                    it.p = MyPointF(sideE*0.5f,-sideE*0.5f)
+                }
+                val vertex8 = Vertex().also {
+                    it.d = Direction.LEFT
+                    it.p = MyPointF(sideE*0.5f,0f)
+                }
+                val vertex9 = Vertex().also {
+                    it.d = Direction.RIGHT
+                    it.p = MyPointF(-sideE*0.5f,0f)
+                }
+                val vertex10 = Vertex().also {
+                    it.d = Direction.STILL
+                    it.p = MyPointF(-sideE*0.5f,-sideE*0.5f)
+                }
+                val vertex11 = Vertex().also {
+                    it.d = Direction.DOWN
+                    it.p = MyPointF(0f,-sideE*0.5f)
+                }
+
+                vertexLst.add(vertex6)
+                vertexLst.add(vertex7)
+                vertexLst.add(vertex8)
+                vertexLst.add(vertex9)
+                vertexLst.add(vertex10)
+                vertexLst.add(vertex11)
             }
         }
+
+        nCnt++
     }
 
     // -------------------------------
     // 描画点を移動する
     // -------------------------------
     private fun movePath() {
-        ratioNow + ratioDiv
-        if (ratioNow >= ratioMax) {
+
+        // 描画点を移動する
+        if (ratioNow > 0f) {
+            vertexLst.forEach {  vertex ->
+                when (vertex.d) {
+                    Direction.RIGHT -> vertex.p.x += sideE*ratioDiv
+                    Direction.LEFT  -> vertex.p.x -= sideE*ratioDiv
+                    Direction.UP    -> vertex.p.y += sideE*ratioDiv
+                    Direction.DOWN  -> vertex.p.y -= sideE*ratioDiv
+                }
+            }
+        }
+
+        // 移動方向を変える
+        if (ratioNow == 0.5f) {
+            vertexLst.forEachIndexed { id, vertex ->
+                vertex.d = when (dirSplit) {
+                    ModeSplit.HORIZONTAL -> {
+                        when (id) {
+                            0 -> Direction.UP
+                            2 -> Direction.RIGHT
+                            3 -> Direction.RIGHT
+                            5 -> Direction.DOWN
+                            6 -> Direction.DOWN
+                            8 -> Direction.LEFT
+                            9 -> Direction.LEFT
+                            11 -> Direction.UP
+                            else -> Direction.STILL
+                        }
+                    }
+                    ModeSplit.VERTICAL -> {
+                        when (id) {
+                            0 -> Direction.LEFT
+                            2 -> Direction.UP
+                            3 -> Direction.UP
+                            5 -> Direction.RIGHT
+                            6 -> Direction.RIGHT
+                            8 -> Direction.DOWN
+                            9 -> Direction.DOWN
+                            11 -> Direction.LEFT
+                            else -> Direction.STILL
+                        }
+                    }
+                }
+            }
+        }
+
+        ratioNow += ratioDiv
+        if (ratioNow > ratioMax) {
             ratioNow = 0f
         }
     }
@@ -250,23 +407,29 @@ class Square2Diamond01Drawable: MyDrawable() {
             (-1..splitN).forEach { col ->
                 canvas.translate(sideE,0f)
 
-                val path = Path()
+                val path1 = Path()
+                val path2 = Path()
                 vertexLst.forEachIndexed { id, vertex ->
-                    when (id) {
-                        0 -> path.moveTo(vertex.p.x,vertex.p.y)
-                        else -> path.lineTo(vertex.p.x,vertex.p.y)
+                    when {
+                        (id == 0) -> path1.moveTo(vertex.p.x,vertex.p.y)
+                        (id >= 1) and (id <= 5) -> path1.lineTo(vertex.p.x,vertex.p.y)
+                        (id == 6) -> path2.moveTo(vertex.p.x,vertex.p.y)
+                        (id >= 7) and (id <= 11) -> path2.lineTo(vertex.p.x,vertex.p.y)
                     }
                 }
-                path.close()
+                path1.close()
+                path2.close()
 
                 when {
                     // 水平モードの場合、"行＋列=偶数"が初期描画する領域
                     (dirSplit == ModeSplit.HORIZONTAL) and ((row+col)%2==0) -> {
-                        canvas.drawPath(path,linePaint)
+                        canvas.drawPath(path1,linePaint)
+                        canvas.drawPath(path2,linePaint)
                     }
                     // 垂直モードの場合、"行＋列=奇数"が初期描画する領域
                     (dirSplit == ModeSplit.VERTICAL) and ((row+col)%2==1) -> {
-                        canvas.drawPath(path,linePaint)
+                        canvas.drawPath(path1,linePaint)
+                        canvas.drawPath(path2,linePaint)
                     }
                 }
             }
@@ -276,7 +439,7 @@ class Square2Diamond01Drawable: MyDrawable() {
 
         // これまでの描画はテンポラリなので、実体にコピーする
         val matrix = Matrix()
-        matrix.setScale(1f,1f)
+        matrix.setScale(1f,-1f)
         imageBitmap = Bitmap.createBitmap(tmpBitmap,0,0,intrinsicWidth,intrinsicHeight,matrix,true)
     }
 
@@ -294,7 +457,6 @@ class Square2Diamond01Drawable: MyDrawable() {
     // Drawable
     // -------------------------------
     override fun setAlpha(alpha: Int) {
-        linePaint.alpha = alpha
     }
 
     // -------------------------------
@@ -306,7 +468,6 @@ class Square2Diamond01Drawable: MyDrawable() {
     // Drawable
     // -------------------------------
     override fun setColorFilter(colorFilter: ColorFilter?) {
-        linePaint.colorFilter = colorFilter
     }
 
     // -------------------------------
